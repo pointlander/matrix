d2a272b3ec0f0a43816199c63c8207975a7057ee
Andrew Snodgrass <pointlander@gmail.com>
2024-01-13 16:00:57 -0600

    Added models

---
+++ gmm.go
@@ -0,0 +1,282 @@
+// Copyright 2023 The Matrix Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package matrix
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"runtime"
+	"sort"
+)
+
+// GMM is a gaussian mixture model
+type GMM struct {
+	Clusters int
+	Window   int
+	Samples  int
+	Rng      *rand.Rand
+}
+
+// NewGMM return a new gmm model
+func NewGMM() GMM {
+	return GMM{
+		Clusters: 20,
+		Window:   8,
+		Samples:  256,
+		Rng:      rand.New(rand.NewSource(3)),
+	}
+}
+
+// GMM is a gaussian mixture model clustering algorithm
+// https://github.com/Ransaka/GMM-from-scratch
+// https://en.wikipedia.org/wiki/Multivariate_normal_distribution
+func (g *GMM) GMM(input Matrix) []int {
+	rng := g.Rng
+	input = Normalize(input)
+
+	type Cluster struct {
+		E RandomMatrix
+		U RandomMatrix
+	}
+	var Pi RandomMatrix
+	mean, stddev, count := float32(0.0), float32(0.0), float32(0.0)
+	for _, value := range input.Data {
+		mean += value
+		count++
+	}
+	mean /= count
+	for _, value := range input.Data {
+		diff := value - mean
+		stddev += diff * diff
+	}
+	stddev /= count
+	stddev = float32(math.Sqrt(float64(stddev)))
+	clusters := make([]Cluster, g.Clusters, g.Clusters)
+	for i := range clusters {
+		clusters[i].E = NewRandomMatrix(input.Cols, input.Cols)
+		for j := range clusters[i].E.Data {
+			clusters[i].E.Data[j].Mean = mean + stddev*float32(rng.NormFloat64())
+			clusters[i].E.Data[j].StdDev = stddev * float32(rng.NormFloat64())
+		}
+		clusters[i].U = NewRandomMatrix(input.Cols, 1)
+		for j := range clusters[i].U.Data {
+			clusters[i].U.Data[j].Mean = mean + stddev*float32(rng.NormFloat64())
+			clusters[i].U.Data[j].StdDev = stddev * float32(rng.NormFloat64())
+		}
+	}
+	Pi = NewRandomMatrix(g.Clusters, input.Rows)
+	for j := range Pi.Data {
+		Pi.Data[j].StdDev = 1
+	}
+
+	type Sample struct {
+		E  []Matrix
+		U  []Matrix
+		C  float64
+		Pi []float64
+	}
+	done, samples, cpus := make(chan bool, 8), make([]Sample, g.Samples, g.Samples), runtime.NumCPU()
+	process := func(j int, seed int64) {
+		rng := rand.New(rand.NewSource(seed))
+		samples[j].Pi = make([]float64, g.Clusters*input.Rows, g.Clusters*input.Rows)
+		for l := 0; l < len(samples[j].Pi); l += g.Clusters {
+			sum := 0.0
+			for k := range clusters {
+				r := Pi.Data[l+k]
+				samples[j].Pi[l+k] = float64(r.StdDev)*rng.NormFloat64() + float64(r.Mean)
+				if samples[j].Pi[l+k] < 0 {
+					samples[j].Pi[l+k] = -samples[j].Pi[l+k]
+				}
+				sum += samples[j].Pi[l+k]
+			}
+			for k := 0; k < g.Clusters; k++ {
+				samples[j].Pi[l+k] /= sum
+			}
+		}
+		cs := make([][]float64, g.Clusters)
+		for k := range cs {
+			cs[k] = make([]float64, input.Rows, input.Rows)
+		}
+		samples[j].E = make([]Matrix, g.Clusters)
+		samples[j].U = make([]Matrix, g.Clusters)
+		samples[j].C = 0
+		for k := range clusters {
+			samples[j].E[k] = clusters[k].E.Sample(rng)
+			samples[j].U[k] = clusters[k].U.Sample(rng)
+
+			d, _ := Determinant(samples[j].E[k])
+			det := float32(d)
+			for f := 0; f < input.Rows; f++ {
+				row := input.Data[f*input.Cols : (f+1)*input.Cols]
+				x := NewMatrix(0, input.Cols, 1)
+				x.Data = append(x.Data, row...)
+				y := MulT(T(MulT(Sub(x, samples[j].U[k]), samples[j].E[k])), Sub(x, samples[j].U[k]))
+				pdf := math.Pow(2*math.Pi, -float64(input.Cols)/2) *
+					math.Pow(float64(det), 1/2) *
+					math.Exp(float64(-y.Data[0]/2))
+				cs[k][f] = samples[j].Pi[f*g.Clusters+k] * pdf
+			}
+		}
+		for f := 0; f < input.Rows; f++ {
+			sum := 0.0
+			for k := range clusters {
+				sum += cs[k][f]
+			}
+			for k := range clusters {
+				cs[k][f] /= sum
+			}
+		}
+		for k := range clusters {
+			mean := 0.0
+			for _, value := range cs[k] {
+				mean += value
+			}
+			mean /= float64(input.Rows)
+			stddev := 0.0
+			for _, value := range cs[k] {
+				diff := value - mean
+				stddev += diff * diff
+			}
+			stddev /= float64(input.Rows)
+			stddev = math.Sqrt(stddev)
+			samples[j].C += math.Exp(-stddev)
+		}
+		done <- true
+	}
+	for i := 0; i < 128; i++ {
+		j, flight := 0, 0
+		for j < g.Samples && flight < cpus {
+			go process(j, rng.Int63())
+			j++
+			flight++
+		}
+		for j < g.Samples {
+			<-done
+			flight--
+
+			go process(j, rng.Int63())
+			j++
+			flight++
+		}
+		for f := 0; f < flight; f++ {
+			<-done
+		}
+
+		aa := make([]Cluster, g.Clusters, g.Clusters)
+		for i := range aa {
+			aa[i].E = NewRandomMatrix(input.Cols, input.Cols)
+			for j := range aa[i].E.Data {
+				aa[i].E.Data[j].StdDev = 0
+			}
+			aa[i].U = NewRandomMatrix(input.Cols, 1)
+			for j := range aa[i].U.Data {
+				aa[i].U.Data[j].StdDev = 0
+			}
+		}
+		pi := NewRandomMatrix(g.Clusters, input.Rows)
+		for j := range pi.Data {
+			pi.Data[j].StdDev = 0
+		}
+
+		sort.Slice(samples, func(i, j int) bool {
+			return samples[i].C < samples[j].C
+		})
+		fmt.Println(samples[0].C)
+
+		weights, sum := make([]float64, g.Window), 0.0
+		for i := range weights {
+			sum += samples[i].C
+			weights[i] = samples[i].C
+		}
+		for i := range weights {
+			weights[i] /= sum
+		}
+
+		for k := range clusters {
+			for i := range samples[:g.Window] {
+				for x := range aa[k].E.Data {
+					value := samples[i].E[k].Data[x]
+					aa[k].E.Data[x].Mean += float32(weights[i]) * value
+				}
+			}
+			for i := range samples[:g.Window] {
+				for x := range aa[k].E.Data {
+					diff := aa[k].E.Data[x].Mean - samples[i].E[k].Data[x]
+					aa[k].E.Data[x].StdDev += float32(weights[i]) * diff * diff
+				}
+			}
+			for x := range aa[k].E.Data {
+				aa[k].E.Data[x].StdDev /= (float32(g.Window) - 1) / float32(g.Window)
+				aa[k].E.Data[x].StdDev = float32(math.Sqrt(float64(aa[k].E.Data[x].StdDev)))
+			}
+
+			for i := range samples[:g.Window] {
+				for x := range aa[k].U.Data {
+					value := samples[i].U[k].Data[x]
+					aa[k].U.Data[x].Mean += float32(weights[i]) * value
+				}
+			}
+			for i := range samples[:g.Window] {
+				for x := range aa[k].U.Data {
+					diff := aa[k].U.Data[x].Mean - samples[i].U[k].Data[x]
+					aa[k].U.Data[x].StdDev += float32(weights[i]) * diff * diff
+				}
+			}
+			for x := range aa[k].U.Data {
+				aa[k].U.Data[x].StdDev /= (float32(g.Window) - 1) / float32(g.Window)
+				aa[k].U.Data[x].StdDev = float32(math.Sqrt(float64(aa[k].U.Data[x].StdDev)))
+			}
+		}
+
+		for i := range samples[:g.Window] {
+			for x := range pi.Data {
+				value := samples[i].Pi[x]
+				pi.Data[x].Mean += float32(weights[i] * value)
+			}
+		}
+		for i := range samples[:g.Window] {
+			for x := range pi.Data {
+				diff := float32(pi.Data[x].Mean) - float32(samples[i].Pi[x])
+				pi.Data[x].StdDev += float32(weights[i]) * diff * diff
+			}
+		}
+		for x := range pi.Data {
+			pi.Data[x].StdDev /= (float32(g.Window) - 1) / float32(g.Window)
+			pi.Data[x].StdDev = float32(math.Sqrt(float64(pi.Data[x].StdDev)))
+		}
+
+		clusters = aa
+		Pi = pi
+	}
+
+	output := make([]int, input.Rows)
+	for i := 0; i < input.Rows; i++ {
+		row := input.Data[i*input.Cols : (i+1)*input.Cols]
+		x := NewMatrix(0, input.Cols, 1)
+		x.Data = append(x.Data, row...)
+
+		index, max := 0, 0.0
+		for j := 0; j < g.Clusters; j++ {
+			sort.Slice(samples, func(i, j int) bool {
+				return samples[i].C < samples[j].C
+			})
+			sample := samples[0]
+
+			d, _ := Determinant(sample.E[j])
+			det := float32(d)
+			y := MulT(T(MulT(Sub(x, sample.U[j]), sample.E[j])), Sub(x, sample.U[j]))
+			pdf := math.Pow(2*math.Pi, -float64(input.Cols)/2) *
+				math.Pow(float64(det), 1/2) *
+				math.Exp(float64(-y.Data[0]/2))
+			pdf *= float64(samples[j].Pi[i*g.Clusters+j])
+			if pdf > max {
+				index, max = j, pdf
+			}
+		}
+		output[i] = index
+	}
+	return output
+}
---
+++ net.go
@@ -0,0 +1,174 @@
+// Copyright 2023 The Matrix Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package matrix
+
+import (
+	"math"
+	"math/rand"
+	"runtime"
+	"sort"
+)
+
+// Net is a net
+type Net struct {
+	Window  int
+	Samples int
+	Inputs  int
+	Outputs int
+	Rng     *rand.Rand
+	Q       RandomMatrix
+	K       RandomMatrix
+	V       RandomMatrix
+}
+
+// NewNet makes a new network
+func NewNet(seed int64, inputs, outputs int) Net {
+	rng := rand.New(rand.NewSource(seed))
+	return Net{
+		Window:  32,
+		Samples: 256,
+		Inputs:  inputs,
+		Outputs: outputs,
+		Rng:     rng,
+		Q:       NewRandomMatrix(inputs, outputs),
+		K:       NewRandomMatrix(inputs, outputs),
+		V:       NewRandomMatrix(inputs, outputs),
+	}
+}
+
+// Sample is a sample of a random neural network
+type Sample struct {
+	Entropy float32
+	Neurons Matrix
+	Outputs Matrix
+	Out     Matrix
+}
+
+// CalculateStatistics calculates the statistics of systems
+func (n Net) CalculateStatistics(systems []Sample) RandomMatrix {
+	statistics := NewRandomMatrix(n.Inputs, n.Outputs)
+	for i := range statistics.Data {
+		statistics.Data[i].StdDev = 0
+	}
+	weights, sum := make([]float64, n.Window), 0.0
+	for i := range weights {
+		weight := math.Exp(-float64(systems[i].Entropy))
+		sum += weight
+		weights[i] = weight
+	}
+	for i := range weights {
+		weights[i] /= sum
+	}
+
+	for i := range systems[:n.Window] {
+		for j, value := range systems[i].Neurons.Data {
+			statistics.Data[j].Mean += float32(weights[i]) * value
+		}
+	}
+	for i := range systems[:n.Window] {
+		for j, value := range systems[i].Neurons.Data {
+			diff := statistics.Data[j].Mean - value
+			statistics.Data[j].StdDev += float32(weights[i]) * diff * diff
+		}
+	}
+	for i := range statistics.Data {
+		statistics.Data[i].StdDev /= (float32(n.Window) - 1.0) / float32(n.Window)
+		statistics.Data[i].StdDev = float32(math.Sqrt(float64(statistics.Data[i].StdDev)))
+	}
+	return statistics
+}
+
+// Fire runs the network
+func (n *Net) Fire(query, key, value Matrix) (float32, Matrix, Matrix, Matrix) {
+	q := NewMatrix(0, n.Outputs, n.Samples)
+	q.Data = q.Data[:n.Outputs*n.Samples]
+	k := NewMatrix(0, n.Outputs, n.Samples)
+	k.Data = k.Data[:n.Outputs*n.Samples]
+	v := NewMatrix(0, n.Outputs, n.Samples)
+	v.Data = v.Data[:n.Outputs*n.Samples]
+	systemsQ := make([]Sample, n.Samples)
+	systemsK := make([]Sample, n.Samples)
+	systemsV := make([]Sample, n.Samples)
+	done, cpus := make(chan bool, 8), runtime.NumCPU()
+	process := func(i int, seed int64) {
+		rng := rand.New(rand.NewSource(seed))
+		{
+			neurons := n.Q.SampleDiscrete(rng)
+			outputs := NewMatrix(0, n.Outputs, 1)
+			out := MulT(neurons, query)
+			copy(q.Data[i*n.Outputs:], out.Data)
+			outputs.Data = append(outputs.Data, out.Data...)
+			systemsQ[i] = Sample{
+				Neurons: neurons,
+				Outputs: outputs,
+			}
+		}
+		{
+			neurons := n.K.SampleDiscrete(rng)
+			outputs := NewMatrix(0, n.Outputs, 1)
+			out := MulT(neurons, key)
+			copy(k.Data[i*n.Outputs:], out.Data)
+			outputs.Data = append(outputs.Data, out.Data...)
+			systemsK[i] = Sample{
+				Neurons: neurons,
+				Outputs: outputs,
+			}
+		}
+		{
+			neurons := n.V.SampleDiscrete(rng)
+			outputs := NewMatrix(0, n.Outputs, 1)
+			out := MulT(neurons, value)
+			copy(v.Data[i*n.Outputs:], out.Data)
+			outputs.Data = append(outputs.Data, out.Data...)
+			systemsV[i] = Sample{
+				Neurons: neurons,
+				Outputs: outputs,
+			}
+		}
+		done <- true
+	}
+	j, flight := 0, 0
+	for j < n.Samples && flight < cpus {
+		go process(j, n.Rng.Int63())
+		j++
+		flight++
+	}
+	for j < n.Samples {
+		<-done
+		flight--
+
+		go process(j, n.Rng.Int63())
+		j++
+		flight++
+	}
+	for f := 0; f < flight; f++ {
+		<-done
+	}
+
+	outputs, entropies := SelfEntropy(q, k, v)
+	for i, entropy := range entropies {
+		systemsQ[i].Entropy = entropy
+		systemsQ[i].Out = outputs[i]
+		systemsK[i].Entropy = entropy
+		systemsK[i].Out = outputs[i]
+		systemsV[i].Entropy = entropy
+		systemsV[i].Out = outputs[i]
+	}
+	sort.Slice(systemsQ, func(i, j int) bool {
+		return systemsQ[i].Entropy < systemsQ[j].Entropy
+	})
+	sort.Slice(systemsK, func(i, j int) bool {
+		return systemsK[i].Entropy < systemsK[j].Entropy
+	})
+	sort.Slice(systemsV, func(i, j int) bool {
+		return systemsV[i].Entropy < systemsV[j].Entropy
+	})
+
+	n.Q = n.CalculateStatistics(systemsQ)
+	n.K = n.CalculateStatistics(systemsK)
+	n.V = n.CalculateStatistics(systemsV)
+
+	return systemsV[0].Entropy, systemsQ[0].Outputs, systemsK[0].Outputs, systemsV[0].Outputs
+}
